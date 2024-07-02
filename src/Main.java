7c87b5f2b67179f65aff26fd3df9e4f9e0bfa3ff
maarcoscarrijo <carrijo.x@hotmail.com>
2024-07-02 20:28:12 -0300

    Feat: implements curso and mentoria
    Signed-off-by: maarcoscarrijo <carrijo.x@hotmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import me.dio.desafio.dominio.Curso;
+import me.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Bootcamp DIO");
+        curso1.setCargaHoraria(87);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Desenvolvendo Java com IA");
+        curso2.setDescricao("Bootcamp DIO");
+        curso2.setCargaHoraria(60);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Mentoria programando com Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
